8e6de73e9a593de085f3fdba0310ab549a9b7638
ArIsh321 <arishme09@gmail.com>
2022-11-30 13:09:31 +0400

    adding sharedpref

---
+++ app/src/main/java/com/test/myplaylist/data/storage/NormalSharedPreferences.kt
@@ -0,0 +1,49 @@
+package com.test.myplaylist.data.storage
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.test.myplaylist.domain.Music
+import javax.inject.Inject
+
+class NormalSharedPreferences @Inject constructor(
+    private val applicationContext: Context,
+    private val defaultSharedPreferences: SharedPreferences
+) : BaseSharedPreferences() {
+
+    init {
+        useDefaultSharedPreferences()
+    }
+
+    fun useDefaultSharedPreferences() {
+        sharedPreferences = defaultSharedPreferences
+    }
+
+    // Use this function for creating a custom sharedPreferences if needed
+    fun useCustomSharedPreferences(name: String) {
+        sharedPreferences = applicationContext.getSharedPreferences(name, Context.MODE_PRIVATE)
+    }
+
+    fun <T> setValue(key: String, value: T) {
+        set(key, value)
+    }
+
+    fun setValueInt(key: String, value: Int) {
+        setInt(key, value)
+    }
+
+    inline fun <reified T> getValue(key: String): T? = get(key) as T?
+
+//    fun setMusicData(music: Music) {
+//        val moshiAdapter = moshi.adapter(Music::class.java)
+//        val data = moshiAdapter.toJson(music)
+//        setValue("Data", data ?: moshiAdapter.toJson(Music()))    }
+//
+//    fun getMusicData(): Music {
+//        val moshiAdapter = moshi.adapter(Music::class.java)
+//        get<String>("Data")?.apply {
+//            if (this.isNotEmpty()) {
+//                return moshiAdapter.fromJson(this) ?: Music()
+//            }
+//        }
+//        return Music()    }
+}
